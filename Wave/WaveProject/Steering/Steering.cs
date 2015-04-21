7b8da31bb1eb64b9a086bfd96f4657ab694aaa7e
tchusami <tchusami_25@hotmail.com>
2015-04-22 01:53:00 +0200

    Operadores steerings

--- Wave/WaveProject/Steering/Steering.cs
+++ Wave/WaveProject/Steering/Steering.cs
@@ -27,5 +27,51 @@ namespace WaveProject.Steering
 
         public abstract void SteeringCalculation(Transform2D target, Transform2D origin, Vector2? characterSpeed = null);
         public abstract void SteeringCalculation(SteeringBehavior target, SteeringBehavior origin, Vector2? characterSpeed = null);
+
+        #region Operadores
+        public static Steering operator +(Steering s1, Steering s2)
+        {
+            Steering result = new NonFuncionalSteering();
+            result.Linear = s1.Linear + s2.Linear;
+            result.Angular = s1.Angular + s2.Angular;
+            return result;
+        }
+        public static Steering operator -(Steering s1, Steering s2)
+        {
+            Steering result = new NonFuncionalSteering();
+            result.Linear = s1.Linear - s2.Linear;
+            result.Angular = s1.Angular - s2.Angular;
+            return result;
+        }
+        public static Steering operator *(Steering s1, Steering s2)
+        {
+            Steering result = new NonFuncionalSteering();
+            result.Linear = s1.Linear * s2.Linear;
+            result.Angular = s1.Angular * s2.Angular;
+            return result;
+        }
+        public static Steering operator /(Steering s1, Steering s2)
+        {
+            Steering result = new NonFuncionalSteering();
+            result.Linear = s1.Linear / (s2.Linear == Vector2.Zero ? Vector2.One : s2.Linear);
+            result.Angular = s1.Angular / (s2.Angular == 0 ? 1f : s2.Angular);
+            return result;
+        }
+        #endregion
+
+        #region SteeringContenedor
+        public class NonFuncionalSteering : Steering
+        {
+            public override void SteeringCalculation(Transform2D target, Transform2D origin, Vector2? characterSpeed = null)
+            {
+                return;
+            }
+
+            public override void SteeringCalculation(SteeringBehavior target, SteeringBehavior origin, Vector2? characterSpeed = null)
+            {
+                return;
+            }
+        }
+        #endregion
     }
 }

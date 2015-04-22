983338f0ac821abfdcc1a5a15d054fde3f7af4f4
tchusami <tchusami_25@hotmail.com>
2015-04-22 19:00:54 +0200

    Velocity matching

---
+++ Wave/WaveProject/Steering/VelocityMatching.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaveEngine.Common.Math;
+using WaveEngine.Framework.Graphics;
+
+namespace WaveProject.Steering
+{
+    public class VelocityMatching : Steering
+    {
+        protected float MaxAceleration = 0.2f;
+        public float TimeToTarget { get; set; }
+
+        public VelocityMatching()
+        {
+            TimeToTarget = 0.1f;
+        }
+
+        public override void SteeringCalculation(Transform2D target, Transform2D origin, Vector2? characterSpeed = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SteeringCalculation(SteeringBehavior target, SteeringBehavior origin)
+        {
+            Linear = target.Speed - origin.Speed;
+            Linear /= TimeToTarget;
+
+            if (Linear.Length() > MaxAceleration)
+            {
+                Linear.Normalize();
+                Linear *= MaxAceleration;
+            }
+
+            Angular = 0f;
+        }
+    }
+}
